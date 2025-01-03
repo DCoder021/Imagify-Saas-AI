94d0e16728aca286c177f9fff5be27a72c15d31b
Sudip Rakshit <88674159+DCoder021@users.noreply.github.com>
2025-01-03 23:13:20 +0530

    Update Login.jsx profile icon

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -75,7 +75,7 @@ const Login = () => {
             <p className='text-sm'>Welcome back! Please sign in to continue</p>
         {state  !== 'Login' &&
             <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
-                <img src={assets.xxxx} alt=""/>
+                <img src={assets.profile_icon} width=20 alt=""/>
                 <input onChange={e => setName(e.target.value)} value={name} type="text" className='outline-none text-sm' placeholder='Full Name' required />
             </div>}
 
